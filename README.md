5159bbb71038d37adc1afd55e640d45fd86b7cf6
Melissa B. Hernández <karenbuenoh1809@gmail.com>
2022-10-03 23:49:30 -0500

    I checked the last exercice :(

--- README.md
+++
@@ -1,2 +0,0 @@
-# RevisionCodigoOtros
-En este ejercicio se revisan los códigos de otras personas, un ejercicio común entre Jrs.
---
+++ filtro.js
@@ -0,0 +1,64 @@
+// Tenemos un li de productos
+
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"}, //añadí la imagen no estaba referenciada
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+];
+
+const li = document.getElementById("lista-de-productos");
+const $i = document.getElementById('input');
+
+for (let i = 0; i < productos.length; i++) {
+  var d = document.createElement("div")
+  d.classList.add("producto")
+
+  var ti = document.createElement("p")
+  ti.classList.add("titulo")
+  ti.textContent = productos[i].nombre
+  
+  var imagen = document.createElement("img");
+  imagen.setAttribute('src', productos[i].img);
+
+  d.appendChild(ti)
+  d.appendChild(imagen)
+
+  li.appendChild(d)
+}
+
+const btn = document.getElementById("btnFiltro"); //Primero que nada se añade el botón con getElementById.
+
+//displayProductos(productos) <No sé para qué es.
+
+btn.onclick = function() {
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
+
+  const texto = $i.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto );
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productosFiltrados[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
+
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
